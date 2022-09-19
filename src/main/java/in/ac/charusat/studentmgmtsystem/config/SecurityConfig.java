13e303d344a3a735c3d78feb69f6e2e800850c92
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2022-09-19 13:02:40 +0530

    auth

--- src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -2,17 +2,18 @@ package in.ac.charusat.studentmgmtsystem.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
-import java.net.PasswordAuthentication;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,25 +35,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 .password("user")
                                 .roles("USER")
                 );
-    //        auth.inMemoryAuthentication()
-//                .withUser("admin")
-//                .password("admin")
-//                .roles("ADMIN")
-//                .and()
-//                .withUser("user")
-//                .password("user")
-//                .roles("USER");
+            auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("user")
+                .roles("USER");
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated()                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncode(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+
 }
