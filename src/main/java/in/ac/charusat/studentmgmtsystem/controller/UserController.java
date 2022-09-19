684b477d710701bdfadf2cda3b4a2bd1c54b0bc1
Harshal Jethwa <89203079+HARSHALJETHWA19@users.noreply.github.com>
2022-09-19 13:21:35 +0530

    usercontroller

---
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/UserController.java
@@ -0,0 +1,46 @@
+package in.ac.charusat.studentmgmtsystem.controller;
+
+import in.ac.charusat.studentmgmtsystem.model.JwtResponse;
+import in.ac.charusat.studentmgmtsystem.model.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin(origins="http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> doLogin(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), loginRequest.getPassword()
+        ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+        UserDetails myUserDetails = (UserDetails) authentication.getPrincipal();
+        List<String> roles = myUserDetails.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());
+        return ResponseEntity.ok(
+                new JwtResponse(jwt,
+                        myUserDetails.getUsername(),
+                        roles)
+        );
+    }
+}
